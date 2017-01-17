c3c77babadc7af1ac17b58314953d4f027f61bb8
leo <leo@sdsol.com>
2017-01-16 22:45:24 -0500

    app logo and lint fixes

--- app/src/main/java/com/acdemo/hashsearch/activities/SearchActivity.java
+++ app/src/main/java/com/acdemo/hashsearch/activities/SearchActivity.java
@@ -11,7 +11,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.acdemo.hashsearch.R;
@@ -84,21 +83,6 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 
